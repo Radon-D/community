ec6fa07b0319b85f120a4ca8baadca65926da7ef
Radon-D <13798358565@163.com>
2019-06-17 20:50:47 +0800

    init reporsitory

---
+++ src/test/java/com/study/radon/community/CommunityApplicationTest.java
@@ -0,0 +1,33 @@
+package com.study.radon.community;
+
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+
+/**
+ * Unit test for simple App.
+ */
+public class CommunityApplicationTest extends TestCase {
+	/**
+	 * Create the test case
+	 *
+	 * @param testName name of the test case
+	 */
+	public CommunityApplicationTest(String testName) {
+		super(testName);
+	}
+
+	/**
+	 * @return the suite of tests being tested
+	 */
+	public static Test suite() {
+		return new TestSuite(CommunityApplicationTest.class);
+	}
+
+	/**
+	 * Rigourous Test :-)
+	 */
+	public void testApp() {
+		assertTrue(true);
+	}
+}
